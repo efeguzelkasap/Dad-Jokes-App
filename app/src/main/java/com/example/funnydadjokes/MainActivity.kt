3da30ab6b52dd093a1a9dbcc42c74f19f3202141
efeguzelkasap <efeguzelkasap@hotmail.com>
2023-04-05 00:29:05 +0100

    Add Admob banner ad

--- app/src/main/java/com/example/funnydadjokes/MainActivity.kt
+++ app/src/main/java/com/example/funnydadjokes/MainActivity.kt
@@ -10,22 +10,60 @@ import android.widget.TextView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.funnydadjokes.repository.Repository
+import com.google.android.gms.ads.*
 
 
 class MainActivity : AppCompatActivity(), OnClickListener {
 
     private lateinit var viewModel: MainViewModel
+    private val TAG = "MainActivity"
+    private lateinit var mAdView: AdView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val jokeBtn = findViewById<Button>(R.id.jokeButton)
 
+        MobileAds.initialize(this)
+
+        bannerAd()
+
         jokeBtn.setOnClickListener(this)
 
         generateJoke()
 
 
+    }
+
+    private fun bannerAd()
+    {
+        mAdView = findViewById(R.id.adView)
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
+
+        mAdView.adListener = object  : AdListener()
+        {
+            override fun onAdLoaded() {
+                Log.d(TAG, "Ad loaded")
+            }
+
+            override fun onAdFailedToLoad(p0: LoadAdError) {
+                Log.d(TAG, "Ad failed to load")
+            }
+
+            override fun onAdOpened() {
+                Log.d(TAG, "Ad opened")
+            }
+
+            override fun onAdClicked() {
+                Log.d(TAG, "Ad loaded")
+            }
+
+            override fun onAdClosed() {
+                Log.d(TAG, "Ad Closed")
+            }
+
+        }
     }
 
     override fun onClick(v: View?) {
